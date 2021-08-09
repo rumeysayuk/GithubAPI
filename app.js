c7631da25b1e814eb5a36b9d638a028de36f1cd7
Rümeysa yük <19110131041@ogr.ksu.edu.tr>
2021-08-09 14:52:32 +0300

    completed.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const clearAllSearches = document.querySelector("#clear-last-users")
 const githubName = document.querySelector("#githubname")
 const lastUsers = document.querySelector("#last-users")
 const github = new Github();
+const ui = new Ui();
+
 eventListeners();
 
 function eventListeners() {
@@ -15,14 +17,40 @@ function getData(e) {
     const username = githubName.value.trim();
     if (username === "") {
         alert("lütfen geçerli bir kullanıcı adı gir!")
+    } else {
+        github.getGithubData(username).then(res => {
+            if (res.user.message === "Not Found") {
+                ui.showError("Kullanıcı bulunamadı..")
+            } else {
+                ui.addSearchedUserToUI(username)
+                Storage.addSearchedUserToLocalStorage(username);
+                ui.showUserInfo(res.user)
+                ui.showUserRepo(res.repo)
+            }
+        })
+            .catch(err => {
+                ui.showError(err)
+            })
     }
-    github.getGithubData(username)
 
+    ui.clearInput()
     e.preventDefault();
 }
 
 function clearAllSearched() {
+    if (confirm("Emin misiniz?")){
+        Storage.clearAllUsersFromLocalStorage();
+        ui.deleteSearchedToUI()
+    }
+
 }
 
 function getAllSearched() {
+let users=Storage.getSearchedUsersFromLocalStorage();
+    //
+    let result="";
+users.forEach(user=>{
+result+=`<li class="list-group-item">${user}</li>`
+});
+lastUsers.innerHTML=result;
 }
--- storage.js
+++ storage.js
@@ -1,7 +1,28 @@
-function getAllSearchedLocalStorage(){
+class Storage {
+    static getSearchedUsersFromLocalStorage() {
+        let users;
+        if (localStorage.getItem("searched") === null) {
+            users = [];
+        } else {
+            users = JSON.parse(localStorage.getItem("searched"));
+        }
+        return users;
+    }
+
+    static addSearchedUserToLocalStorage(username) {
+        let users = this.getSearchedUsersFromLocalStorage();
+
+        if (users.indexOf(username) === -1) {
+            users.push(username)
+        }
+        localStorage.setItem("searched", JSON.stringify(users))
+    }
+
+    static clearAllUsersFromLocalStorage() {
+        localStorage.removeItem("searched")
+    }
 
 }
 
-function clearAllLocalStorage(){
 
-}
+
